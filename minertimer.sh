24b6c9eed014d7373839dd4faee555cb4556818c
Christian Lackas <christian@lackas.net>
2025-12-01 11:05:23 +0100

    Added support for online script to approve more time

--- minertimer.sh
+++ minertimer.sh
@@ -6,12 +6,14 @@
 ###
 
 # Time limit in seconds (e.g., 1800 for half an hour)
-TIME_LIMIT=1800
+TIME_LIMIT_DEFAULT=1800
 DISPLAY_5_MIN_WARNING=true
 DISPLAY_1_MIN_WARNING=true
+NOTIFICATION_URL="http://minecraft.lackas.net/timer"
 
 # Directory and file to store total played time for the day
-LOG_DIRECTORY="/var/lib/minertimer"
+#LOG_DIRECTORY="/var/lib/minertimer"
+LOG_DIRECTORY="."
 LOG_FILE="${LOG_DIRECTORY}/minertimer_playtime.log"
 
 # Create the directory (don't throw error if already exists)
@@ -20,30 +22,54 @@ mkdir -p $LOG_DIRECTORY
 # Get the current date
 CURRENT_DATE=$(date +%Y-%m-%d)
 
+TIME_LIMIT="$TIME_LIMIT_DEFAULT"
+TOTAL_PLAYED_TIME="0"
+
+write_log() {
+    echo "$CURRENT_DATE" > "$LOG_FILE"
+    echo "$TOTAL_PLAYED_TIME" >> "$LOG_FILE"
+    echo "$TIME_LIMIT" >> "$LOG_FILE"
+}
+
+
 # Read the last play date and total played time from the log file
 if [ -f "$LOG_FILE" ]; then
-    LAST_PLAY_DATE=$(head -n 1 "$LOG_FILE")
-    TOTAL_PLAYED_TIME=$(tail -n 1 "$LOG_FILE")
+    exec 3< $LOG_FILE
+    read -r LAST_PLAY_DATE <&3
+    read -r TOTAL_PLAYED_TIME <&3
+    read -r TIME_LIMIT <&3
 else
     LAST_PLAY_DATE="$CURRENT_DATE"
     TOTAL_PLAYED_TIME=0
-    echo "$CURRENT_DATE" > "$LOG_FILE"
-    echo "0" >> "$LOG_FILE"
+    write_log
 fi
 
  # If it's a new day, or first use, reset the playtime
 if [ "$LAST_PLAY_DATE" != "$CURRENT_DATE" ]; then
     TOTAL_PLAYED_TIME=0
-    echo "$CURRENT_DATE" > "$LOG_FILE"
-    echo "0" >> "$LOG_FILE"
+    TIME_LIMIT="$TIME_LIMIT_DEFAULT"
+    write_log
 fi
+
+RECHECK_TIME=2
 
 while true; do
     
     MINECRAFT_PIDS=$(ps aux | grep -iww "[M]inecraft" | awk '{print $2}')
+    MINECRAFT_UID=$(ps aux | grep -iww "[M]inecraft" | awk '{print $1}')
     # If Minecraft is running
     
     if [ -n "$MINECRAFT_PIDS" ]; then
+        if [ -n "$NOTIFICATION_URL" ]; then
+            # echo $url
+            url="$NOTIFICATION_URL/$MINECRAFT_UID/$CURRENT_DATE/$TOTAL_PLAYED_TIME/$TIME_LIMIT"
+            res=`curl -s $url`
+            # echo "RESPONSE: $res"
+            if [[ $? -eq 0 && "$res" =~ ^[0-9]+$ && $res -ne $TIME_LIMIT && $res -gt $TOTAL_PLAYED_TIME ]]; then
+                echo "Increasing TIME_LIMIT from $TIME_LIMIT to $res ($TOTAL_PLAYED_TIME played)"
+                TIME_LIMIT="$res"
+            fi
+        fi 
         
         # If the time limit has been reached, kill the Minecraft process
         if ((TOTAL_PLAYED_TIME >= TIME_LIMIT)); then
@@ -62,14 +88,16 @@ while true; do
         fi
         
         # Sleep, then increment the playtime
-        sleep 20
-        TOTAL_PLAYED_TIME=$((TOTAL_PLAYED_TIME + 20))
+        sleep $RECHECK_TIME
+        TOTAL_PLAYED_TIME=$((TOTAL_PLAYED_TIME + $RECHECK_TIME))
 
-        # Update the total played time in the log file (Note on mac -i requires extension)
-        sed -i '' "$ s/.*/$TOTAL_PLAYED_TIME/" "$LOG_FILE"
+        write_log
+
+        # # Update the total played time in the log file (Note on mac -i requires extension)
+        # sed -i '' "$ s/.*/$TOTAL_PLAYED_TIME/" "$LOG_FILE"
 
     else
-        sleep 10
+        sleep $RECHECK_TIME
     fi
 
     # Get the current date
@@ -86,10 +114,12 @@ while true; do
     # If it's a new day, reset the playtime
     if [ "$LAST_PLAY_DATE" != "$CURRENT_DATE" ]; then
         TOTAL_PLAYED_TIME=0
+        TIME_LIMIT="$TIME_LIMIT_DEFAULT"
         DISPLAY_5_MIN_WARNING=true
         DISPLAY_1_MIN_WARNING=true
-        echo "$CURRENT_DATE" > "$LOG_FILE"
-        echo "0" >> "$LOG_FILE"
-        echo "RESET DATE - $CURRENT_DATE"
+        write_log
     fi
 done
+
+
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
